1b1337fab61a5f5ce103162109d0426bb4bd0029
jing01.he <849679119@qq.com>
2015-08-16 15:31:18 +0800

    javapro

---
+++ javaproject/src/com/test/vip/Test.java
@@ -0,0 +1,18 @@
+package com.test.vip;
+
+public class Test {
+
+	/**
+	 * @param args
+	 * @see <a>
+	 * href="http://www.baidu.com"
+	 *</a>
+	 */
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		System.out.println("this is my main");
+
+	}
+
+}
